7344066c35fc6c60ffe47c8d897d107aebefd45a
morozzov <romm858@mail.ru>
2022-11-10 20:54:38 +0300

    Profile work started.

--- StudentRating/src/main/java/com/example/studentrating/controllers/UsersController.java
+++ StudentRating/src/main/java/com/example/studentrating/controllers/UsersController.java
@@ -1,9 +1,17 @@
 package com.example.studentrating.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/users")
 public class UsersController {
+
+    @GetMapping("/getById")
+    public String getAll(Model model) {
+        model.addAttribute("title", "Профиль");
+        return "profile";
+    }
 }
