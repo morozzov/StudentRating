df58be69a0a0096e29f5505d0ea144cca72e4256
morozzov <romm858@mail.ru>
2022-11-20 20:47:14 +0300

    Renaming

--- StudentRating/src/main/java/com/example/studentrating/models/PastYearPoint.java
+++ StudentRating/src/main/java/com/example/studentrating/models/PastYearPoint.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Getter
 @Setter
-public class PastYearPoints {
+public class PastYearPoint {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
--- StudentRating/src/main/java/com/example/studentrating/repositories/PastYearPointRepository.java
+++ StudentRating/src/main/java/com/example/studentrating/repositories/PastYearPointRepository.java
@@ -1,12 +1,12 @@
 package com.example.studentrating.repositories;
 
-import com.example.studentrating.models.PastYearPoints;
+import com.example.studentrating.models.PastYearPoint;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.ArrayList;
 
-public interface PastYearPointRepository extends CrudRepository<PastYearPoints, Long> {
+public interface PastYearPointRepository extends CrudRepository<PastYearPoint, Long> {
 
-    ArrayList<PastYearPoints> findAllByStudent_Id(Sort year, Long studentId);
+    ArrayList<PastYearPoint> findAllByStudent_Id(Sort year, Long studentId);
 }
