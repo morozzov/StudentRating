14093624562f5f4fa7e3fd0411a8db2e7d9bc65c
morozzov <romm858@mail.ru>
2022-11-20 20:48:25 +0300

    Removed excess imports

--- StudentRating/src/main/java/com/example/studentrating/models/Student.java
+++ StudentRating/src/main/java/com/example/studentrating/models/Student.java
@@ -6,12 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.PrePersist;
-import javax.persistence.PreUpdate;
-import java.time.LocalDateTime;
 
 @Entity
 @AllArgsConstructor
