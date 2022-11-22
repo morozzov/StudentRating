a4bf3b587d13e07f3b6228a94f4a23793146ae3e
morozzov <romm858@mail.ru>
2022-11-22 17:55:45 +0300

    StudentsRestController added
    Methods moved from ApiController to StudentsRestController.
    Links in views updated.

--- StudentRating/src/main/java/com/example/studentrating/controllers/ApiController.java
+++ StudentRating/src/main/java/com/example/studentrating/controllers/ApiController.java
@@ -25,15 +25,15 @@ public class ApiController {
 
     @Autowired
     private NotificationRepository notificationRepository;
-
-    @GetMapping("/getById/{id}")
-    public StudentDTO getById(@PathVariable("id") Long id) {
-        Student student = studentRepository.findById(id).get();
-
-        StudentDTO studentDTO = new StudentDTO(student);
-
-        return studentDTO;
-    }
+//
+//    @GetMapping("/getById/{id}")
+//    public StudentDTO getById(@PathVariable("id") Long id) {
+//        Student student = studentRepository.findById(id).get();
+//
+//        StudentDTO studentDTO = new StudentDTO(student);
+//
+//        return studentDTO;
+//    }
 
     @GetMapping("/getAll")
     public ArrayList<StudentDTO> getAll() {
@@ -55,82 +55,5 @@ public class ApiController {
         StudentDTO studentDTO = new StudentDTO(student);
 
         return studentDTO;
-    }
-
-    @PostMapping("/signIn")
-    public String signIn(String login, String password, HttpSession session) {
-        Student student = studentRepository.findByLoginAndPassword(login, encryptText(password));
-        if (student != null) {
-            setSession(session, student.getId(), "STUDENT");
-            return "success";
-        } else {
-            return "error";
-        }
-    }
-
-    @PostMapping("/signOut")
-    public String signOut(HttpSession request) {
-        try {
-            request.setAttribute("id", null);
-            request.setAttribute("type", null);
-            return "success";
-        } catch (Exception e) {
-            return "e.getMessage()";
-        }
-    }
-
-    @PostMapping("/updateProfile")
-    public String updateProfile(String login, String password1, HttpSession session) {
-        try {
-            // image - null, password1 = ""
-            if (login != "" || password1 != "") {
-                Student student = studentRepository.findById(getSessionId(session)).get();
-                if (login != "") student.setLogin(login);
-                if (password1 != "") student.setPassword(encryptText(password1));
-                studentRepository.save(student);
-            }
-            return "success";
-        } catch (Exception e) {
-            return "e.getMessage()";
-        }
-    }
-
-    @DeleteMapping("/deleteNotification")
-    public String deleteNotification(Long notificationId) {
-        try {
-            notificationRepository.deleteById(notificationId);
-            return "success";
-        } catch (Exception e) {
-            return "e.getMessage()";
-        }
-    }
-
-    private void setSession(HttpSession request, Long id, String type) {
-        request.setAttribute("id", id);
-        request.setAttribute("type", type);
-    }
-
-    public Long getSessionId(HttpSession request) {
-        return (Long) request.getAttribute("id");
-    }
-
-    private static String encryptText(String input) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-512");
-
-            byte[] messageDigest = md.digest(input.getBytes());
-
-            BigInteger no = new BigInteger(1, messageDigest);
-
-            String hashText = no.toString(16);
-
-            while (hashText.length() < 32) {
-                hashText = "0" + hashText;
-            }
-
-            return hashText;
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
---
+++ StudentRating/src/main/java/com/example/studentrating/controllers/StudentsRestController.java
@@ -0,0 +1,103 @@
+package com.example.studentrating.controllers;
+
+import com.example.studentrating.models.Student;
+import com.example.studentrating.repositories.NotificationRepository;
+import com.example.studentrating.repositories.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+@RestController
+@RequestMapping(path = "/studentsRest")
+public class StudentsRestController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Autowired
+    private NotificationRepository notificationRepository;
+
+    @PostMapping("/signIn")
+    public String signIn(String login, String password, HttpSession session) {
+        Student student = studentRepository.findByLoginAndPassword(login, encryptText(password));
+        if (student != null) {
+            setSession(session, student.getId(), "STUDENT");
+            return "success";
+        } else {
+            return "error";
+        }
+    }
+
+    @PostMapping("/signOut")
+    public String signOut(HttpSession request) {
+        try {
+            request.setAttribute("id", null);
+            request.setAttribute("type", null);
+            return "success";
+        } catch (Exception e) {
+            return "e.getMessage()";
+        }
+    }
+
+    @PostMapping("/updateProfile")
+    public String updateProfile(String login, String password1, HttpSession session) {
+        try {
+            // image - null, password1 = ""
+            if (login != "" || password1 != "") {
+                Student student = studentRepository.findById(getSessionId(session)).get();
+                if (login != "") student.setLogin(login);
+                if (password1 != "") student.setPassword(encryptText(password1));
+                studentRepository.save(student);
+            }
+            return "success";
+        } catch (Exception e) {
+            return "e.getMessage()";
+        }
+    }
+
+    @DeleteMapping("/deleteNotification")
+    public String deleteNotification(Long notificationId) {
+        try {
+            notificationRepository.deleteById(notificationId);
+            return "success";
+        } catch (Exception e) {
+            return "e.getMessage()";
+        }
+    }
+
+    private void setSession(HttpSession request, Long id, String type) {
+        request.setAttribute("id", id);
+        request.setAttribute("type", type);
+    }
+
+    public Long getSessionId(HttpSession request) {
+        return (Long) request.getAttribute("id");
+    }
+
+    private static String encryptText(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+
+            byte[] messageDigest = md.digest(input.getBytes());
+
+            BigInteger no = new BigInteger(1, messageDigest);
+
+            String hashText = no.toString(16);
+
+            while (hashText.length() < 32) {
+                hashText = "0" + hashText;
+            }
+
+            return hashText;
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
