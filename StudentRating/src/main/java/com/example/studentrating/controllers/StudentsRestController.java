e8d20deaf39ae6e1edf39d18ede13409fea61bb5
morozzov <romm858@mail.ru>
2022-11-30 22:56:53 +0300

    Update StudentsRestController
    Default password encryption

--- StudentRating/src/main/java/com/example/studentrating/controllers/StudentsRestController.java
+++ StudentRating/src/main/java/com/example/studentrating/controllers/StudentsRestController.java
@@ -58,7 +58,7 @@ public class StudentsRestController {
                 student.setLogin(login);
                 student.setGroupName(group);
                 student.setStudentCouncil(isStudentCouncil);
-                student.setPassword("3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2");
+                student.setPassword(encryptText("123"));
                 student.setImageUrl("/img/avatars/personal.svg");
                 student.setRole("STUDENT");
                 student.setPoints(0);
