1bed0283fcb3db1818b82f8ce87d0c184b2a1526
morozzov <romm858@mail.ru>
2022-11-30 23:36:33 +0300

    Added teacher creation

--- StudentRating/src/main/java/com/example/studentrating/controllers/TeachersRestController.java
+++ StudentRating/src/main/java/com/example/studentrating/controllers/TeachersRestController.java
@@ -1,6 +1,7 @@
 package com.example.studentrating.controllers;
 
 import com.example.studentrating.models.Student;
+import com.example.studentrating.models.Teacher;
 import com.example.studentrating.repositories.StudentRepository;
 import com.example.studentrating.repositories.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,29 +23,24 @@ public class TeachersRestController {
 
     @Autowired
     private TeacherRepository teacherRepository;
-    
+
     @PostMapping("/addNew")
-    public String addNew(String surname, String name, String patronymic, String login, String group, @RequestParam(defaultValue = "false") boolean isStudentCouncil, HttpSession session) {
+    public String addNew(String surname, String name, String patronymic, String login, HttpSession session) {
         try {
             if (studentRepository.findByLogin(login) == null && teacherRepository.findByLogin(login) == null) {
-                Student student = new Student();
-                student.setSurname(surname);
-                student.setName(name);
-                student.setPatronymic(patronymic);
-                student.setLogin(login);
-                student.setGroupName(group);
-                student.setStudentCouncil(isStudentCouncil);
-                student.setPassword(encryptText("123"));
-                student.setImageUrl("/img/avatars/personal.svg");
-                student.setRole("STUDENT");
-                student.setPoints(0);
-                studentRepository.save(student);
+                Teacher teacher = new Teacher();
+                teacher.setSurname(surname);
+                teacher.setName(name);
+                teacher.setPatronymic(patronymic);
+                teacher.setLogin(login);
+                teacher.setPassword(encryptText("123"));
+                teacher.setImageUrl("/img/avatars/personal.svg");
+                teacher.setRole("TEACHER");
+                teacherRepository.save(teacher);
                 return "success";
             } else {
                 return "Введите другой логин";
             }
-
-//            return student;
         } catch (Exception e) {
             return e.getMessage();
         }
